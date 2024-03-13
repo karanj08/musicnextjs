6b3eb5e2eba1c41f8485b30a16883cfcdd7f20e4
karan <karanjaink8899@gmail.com>
2024-03-13 23:40:37 +0530

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import FeaturedCourses from "@/Components/FeaturedCourses";
+import HeroSection from "@/Components/HeroSection";
+import MusicSchoolTestimonials from "@/Components/TestimonialCards";
+import UpcomingWebinar from "@/Components/UpcomingWebinar";
+import WhyChooseUs from "@/Components/WhyChooseUs";
+import Tooltip from "@/Components/Tooltip";
+import Footer from "@/Components/Footer";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"></main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinar />
+      <Tooltip />
+      <Footer />
+    </main>
   );
 }
